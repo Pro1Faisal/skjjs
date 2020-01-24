a8f241d84c67ab39b173fe88f8987857b514ee1a
Pro1Faisal <60229976+Pro1Faisal@users.noreply.github.com>
2020-01-23 19:30:55 -0500

    Create pro1faisal.py

---
+++ pro1faisal.py
@@ -0,0 +1,24 @@
+import os 
+os.system("clear")
+print ("Hello")
+print ("Welcome to my tool")
+print ("Welcome to my tool")
+print ("pro1 Faisal")
+print(12345)
+print(""Hello)
+pr1 = ("pro1faisal")
+print(pr1)
+
+pr1 =12345
+print  (pr1)
+
+pr11 = ("hehisiiefefifjjdjd")
+print (pr11)
+
+flo = 12.78
+print(flo)
+#this code is print pro1faisal
+print("pro1 Faisal")
+
+pr1 =input ("type your name:")
+print ("Your Name Is: " +pr1)
